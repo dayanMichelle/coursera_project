f547b0ffdb1489c52e83da3af6d72bb882c6e456
Dayan <dayaara6@gmail.com>
2023-03-10 12:40:30 -0300

    logic of select time

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,23 +1,51 @@
 import React, { useState } from "react";
 import "./booking.css";
-export const BookingForm = () => {
-  const [date, setDate] = useState("")
-  const [nGuests, setNGestst] = useState("")
-  const [occasion, setOccasion] = useState("")
-  const [time, setTime] = useState(["17:00","18:00","19:00","20:00","21:00","22:00"])
+export const BookingForm = ({ availableTimes, dispatch }) => {
+  const [date, setDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+  const [nGuests, setNGestst] = useState("");
+  const [occasion, setOccasion] = useState("");
 
   return (
     <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
       <label for="res-date">Choose date</label>
-      <input value={date} onChange={(e)=> setDate(e.target.value)} type="date" id="res-date" />
+      <input
+        value={date}
+        onChange={(e) => {
+          setDate(e.target.value);
+          dispatch({ type: "AVAILABLE_TIMES", payload: e.target.value });
+        }}
+        type="date"
+        id="res-date"
+      />
       <label for="res-time">Choose time</label>
-      <select value={time} onChange={(e)=> setTime(e.target.value)}  id="res-time ">
-        {time.map(t=> (<option>{t}</option>))}
+      <select
+        value={selectedTime}
+        onChange={(e) => {
+          setSelectedTime(e.target.value);
+        }}
+        id="res-time "
+      >
+        {availableTimes.map((t) => (
+          <option>{t}</option>
+        ))}
       </select>
       <label htmlFor="guests">Number of guests</label>
-      <input value={nGuests} onChange={(e)=> setNGestst(e.target.value)}  type="number" placeholder="1" min="1" max="10" id="guests" />
+      <input
+        value={nGuests}
+        onChange={(e) => setNGestst(e.target.value)}
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+      />
       <label htmlFor="occasion">Occasion</label>
-      <select value={occasion} onChange={(e)=> setOccasion(e.target.value)}  id="occasion">
+      <select
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+        id="occasion"
+      >
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import { BookingForm } from './BookingForm'
+import React, { useReducer } from "react";
+import { BookingForm } from "./BookingForm";
+
+function updateTimes(state, action) {
+  // action.type => "texto que dice el nombre de la accion" => "availableTimes"
+  // action.payload => "data que recibimos desde el dispatch" => "13/02/2024"
+
+  switch (action.type) {
+    case "AVAILABLE_TIMES":
+    // if (action.payload === "2023-03-15") return ["17:00", "18:00", "19:00"];
+      return state;
+    default:
+      return state;
+  }
+}
+
+const initializeTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 
 export const BookingPage = () => {
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+
   return (
-    <BookingForm></BookingForm>
-  )
-}
+    <BookingForm
+      availableTimes={availableTimes}
+      dispatch={dispatch}
+    ></BookingForm>
+  );
+};
